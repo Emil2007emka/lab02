288f46425cfc539ab0e089d2f36c5c1d2f7d5f35
Emil2007emka <emilrzaev28@gmail.com>
2026-03-20 21:22:27 +0300

    apply Mozilla code style to hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,9 +2,9 @@
 #include <string>
 
 int main() {
-    std::string name;                     // user name variable
-    std::cout << "Enter your name: ";     // prompt
-    std::cin >> name;                     // read input
+    std::string name; // user name variable
+    std::cout << "Enter your name: "; // prompt
+    std::cin >> name; // read input
     std::cout << "Hello world from " << name << std::endl; // output
     return 0;
 }
